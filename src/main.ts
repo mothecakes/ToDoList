765a12665020873b8880b38a5ba230f0ded38eda
mothecakes <boneyh21@gmail.com>
2024-10-15 01:27:28 -0700

    implement basic class functions for projects

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { Project } from "./project";
 
 class TaskManager {
     projects: Project[] = [];
+
+    createProject = function(name : string, description: string) {
+        const project = new Project(name, description);
+        this.projects.push(project);
+    }
+
     createTask = function() {
         const userInput = queryUserTask();
         const userTask = new Task(userInput.title, userInput.description, userInput.date, userInput.priority);
         const projectSelect : number = 0;
-        this.projects[projectSelect].push(userTask);
+        this.projects[projectSelect];
         // TODO  implement priority shifting when splicing in object.
     }
 
